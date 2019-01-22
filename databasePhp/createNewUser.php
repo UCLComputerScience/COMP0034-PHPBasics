c6841c0fe57b3754c4517169b13f44acf0d73f8a
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2019-01-22 16:33:50 +0000

    Added logic to check if user exists before creating

--- databasePhp/createNewUser.php
+++ databasePhp/createNewUser.php
@@ -4,19 +4,27 @@ require_once('phpDatabaseConnection.php');
 $firstName = $_POST['firstName'] ?? '1'; //PHP 7.0
 $familyName = $_POST['familyName'] ?? '1';
 
-$query = "INSERT INTO Users (firstName, familyName) VALUES (";
-$query .= "'" . $firstName . "', '" . $familyName ."')";
+$qryAdd = "INSERT INTO Users (firstName, familyName) VALUES (";
+$qryAdd .= "'" . $firstName . "', '" . $familyName . "')";
+
+$qryFind = "SELECT * FROM Users ";
+$qryFind .= "WHERE firstName = '" . $firstName . "' AND familyName = '" . $familyName . "'";
 
 $connection = connectToDb();
 
-$result = mysqli_query($connection, $query);
-
-// check the query worked
-if($result) {
-    echo "New user created";
-    closeDb($connection);
+//Check if the name exists
+$result = mysqli_query($connection, $qryFind);
+if (mysqli_num_rows($result) > 0) {
+    echo "Sorry... your details are already registered";
 } else {
-    echo mysqli_error($connection);
-    closeDb($connection);
-    exit;
+    $result = mysqli_query($connection, $qryAdd);
+    // check the query worked
+    if ($result) {
+        echo "New user created";
+        closeDb($connection);
+    } else {
+        echo mysqli_error($connection);
+        closeDb($connection);
+        exit;
+    }
 }
